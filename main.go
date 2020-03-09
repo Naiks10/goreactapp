405a182968c658d9681a83eda29b6988481b33f0
naiks10 <ultravirus661@gmail.com>
2020-03-09 12:45:39 +0300

    Secutity changes

--- main.go
+++ main.go
@@ -4,17 +4,15 @@ import (
 	"fmt"
 	"goreactapp/routes"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
 
-//#--main--#//
-
+//Main function => Entry point
 func main() {
 	fmt.Println("listening default port")
 
-	port := os.Getenv("PORT") //for Heroku hosting
+	port := "1012" //os.Getenv("PORT") //for Heroku hosting
 
 	defer func() {
 		fmt.Println("Server Stopped!")
@@ -22,6 +20,11 @@ func main() {
 
 	r := mux.NewRouter()
 	routes.Register(r)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./build/")))
+
+	//r.PathPrefix("/").Handler(http.FileServer(http.Dir("./build/")))
+	r.PathPrefix("/data/").Handler(http.StripPrefix("/data/", http.FileServer(http.Dir("./data/"))))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./build/static/"))))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./build/assets/"))))
+	r.HandleFunc("/", routes.IndexHandler)
 	http.ListenAndServe(":"+port, r)
 }
