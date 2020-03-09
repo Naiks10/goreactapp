4dc48e7b071d25c361c13f9f5d5092eb8e79bdc4
naiks10 <ultravirus661@gmail.com>
2020-03-09 12:49:38 +0300

    Added new SQL query Functions

--- database/functions/functions.go
+++ database/functions/functions.go
@@ -8,54 +8,149 @@ var postgres = sqrl.StatementBuilder.PlaceholderFormat(sqrl.Dollar)
 
 var (
 	//#--ROLES--#//
-	SelectRoles, _, _ = postgres.Select("*").From("roles").ToSql()
+	SelectRoles = postgres.Select("*").From("roles") //+
 	//#--USERS--#//
-	SelectUsers, _, _ = postgres.Select(
-		"user_login", "user_password", "user_surname", "user_name", "user_midname", "user_birthdate", "user_phone", "user_email", "role_id", "role_name").
+	SelectUsers = postgres.Select( //+
+		"user_login",
+		"first_name",
+		"middle_name",
+		"sur_name",
+		"birth_date",
+		"phone_num",
+		"email_addr",
+		"user_image_src",
+		"role_id",
+		"role_name").
 		From("users").
 		Join("roles ON user_role = role_id").
-		ToSql()
+		Where("logical_delete_status = false")
 	//#--ORGANISATIONS--#//
-	SelectOrgs, _, _ = postgres.Select("*").From("organisations").ToSql()
+	SelectOrgs = postgres.Select("*").From("organisations") //+
 	//#--CLIENTS--#//
-	SelectClients, _, _ = postgres.Select(
-		"user_login", "user_surname", "user_name", "user_midname", "user_birthdate", "user_phone", "user_email",
-		"role_id", "role_name", "organisations.organisation_id", "organisations.organisation_name", "organisation_data").
+	SelectClients = postgres.Select(
+		"user_login",
+		"sur_name",
+		"first_name",
+		"middle_name",
+		"birth_date",
+		"phone_num",
+		"email_addr",
+		"user_image_src",
+		"role_id",
+		"role_name",
+		"organisation_id",
+		"full_name",
+		"short_name",
+		"organisation_desc",
+		"organisation_image_src").
 		From("clients").
-		Join("users ON user_login = client_user_login").
+		Join("users ON user_login = client_login").
 		Join("roles on user_role = role_id").
-		Join("organisations on clients.organisation_id = organisations.organisation_id").
-		ToSql()
+		Join("organisations on client_organisation_id = organisation_id")
 	//#--GROUPS--#//
-	SelectWorkGroups, _, _ = postgres.Select("*").From("workgroups").ToSql()
+	SelectWorkGroups = postgres.Select("*").From("workgroups") //+
 	//#--DEVELOPERS--#//
-	SelectDevs, _, _ = postgres.
-				Select("user_login", "user_surname", "user_name", "user_midname", "user_birthdate", "user_phone", "user_email", "role_id", "role_name", "workgroups.workgroup_id", "workgroup_name", "is_general").
-				From("developers").
-				Join("workgroups ON workgroups.workgroup_id = developers.workgroup_id").
-				Join("users ON users.user_login = developers.developer_user_login").
-				Join("roles ON users.user_role = role_id").
-				ToSql()
+	SelectDevs = postgres.Select(
+		"user_login",
+		"sur_name",
+		"first_name",
+		"middle_name",
+		"birth_date",
+		"phone_num",
+		"email_addr",
+		"user_image_src",
+		"outsource_spec",
+		"role_id",
+		"role_name").
+		From("developers").
+		Join("users ON user_login = developer_login").
+		Join("roles ON user_role = role_id")
+	/*SelectDevGroups, _, _ = postgres.Select(
+		"list_id",
+		"developer_login"
+	)*/
 	//#--STATUS--#//
-	SelectStatus, _, _ = postgres.Select("*").From("project_statuses").ToSql()
+	SelectStatus = postgres.Select("*").From("status") //+
 	//#--PROJECTS--#//
-	SelectProjects, _, _ = postgres.Select(
-		"project_id", "cost", "project_info",
+	SelectProjects = postgres.Select(
+		"project_id", "project_name", "project_info",
 		"workgroup_id", "workgroup_name",
-		"project_statuses.project_status_id", "project_statuses.project_status_name",
-		"project_data",
-		"mn.user_login", "mn.user_surname", "mn.user_name", "mn.user_midname", "mn.user_birthdate", "mn.user_phone", "mn.user_email",
-		"ms.user_login as user_login1", "ms.user_surname as user_surname1", "ms.user_name as user_name1", "ms.user_midname as user_midname1", "ms.user_birthdate as user_birthdate1", "ms.user_phone as user_phone1", "ms.user_email as user_email1",
-		"organisations.organisation_id", "organisations.organisation_name", "organisation_data").
+		"status.status_id", "status.status_name",
+		"mn.user_login", "mn.sur_name", "mn.first_name", "mn.middle_name", "mn.birth_date", "mn.phone_num", "mn.email_addr", "mn.user_image_src",
+		"ms.user_login as user_login1", "ms.sur_name as sur_name1", "ms.first_name as first_name1", "ms.middle_name as middle_name1", "ms.birth_date as birth_date1", "ms.phone_num as phone_num1", "ms.email_addr as email_addr1", "ms.user_image_src as user_image_src1",
+		"organisations.organisation_id", "organisations.short_name", "organisations.organisation_image_src").
 		From("projects").
 		Join("workgroups ON workgroups.workgroup_id = projects.project_workgroup_id").
-		Join("clients ON clients.client_user_login = projects.client_user_login").
-		Join("managers ON managers.manager_user_login = projects.manager_user_login").
-		Join("users mn on projects.manager_user_login = mn.user_login").
-		Join("users ms on projects.client_user_login = ms.user_login").
-		Join("organisations on clients.organisation_id = organisations.organisation_id").
-		Join("project_statuses ON project_statuses.project_status_id = projects.project_status_id").
-		ToSql()
+		Join("clients ON clients.client_login = projects.project_client_login").
+		Join("managers ON managers.manager_login = projects.project_manager_login").
+		Join("users mn on projects.project_manager_login = mn.user_login").
+		Join("users ms on projects.project_client_login = ms.user_login").
+		Join("organisations on clients.client_organisation_id = organisations.organisation_id").
+		Join("status ON status.status_id = projects.project_status_id")
+	SelectProjectsPreview = postgres.Select(
+		"project_id",
+		"project_name",
+		"organisation_id",
+		"project_status_id",
+		"status_name",
+		"organisation_image_src",
+		`(
+            SELECT  COUNT(*)
+              FROM  (
+                  SELECT *FROM tasks
+                   WHERE  task_stage_id IN (
+                       SELECT  stage_id 
+                         FROM  stages 
+                        WHERE  stage_module_id IN (
+                            SELECT  module_id 
+                              FROM  modules
+                             WHERE  module_project_id = project_id
+                        )
+                       )
+              ) AS tasks_2
+		) AS tasks_all`,
+		`(
+            SELECT  COUNT(*)
+              FROM  (
+                  SELECT *FROM tasks
+                   WHERE  task_stage_id IN (
+                       SELECT  stage_id 
+                         FROM  stages 
+                        WHERE  stage_module_id IN (
+                            SELECT  module_id 
+                              FROM  modules
+                             WHERE  module_project_id = project_id
+                        )
+                       ) AND task_status_id = 4
+              ) AS tasks_2
+		) AS tasks_finished`,
+		`(
+            SELECT  COUNT(*)
+              FROM  (
+                  SELECT *FROM issues_list
+                   WHERE  task_id IN (
+                       SELECT  task_id 
+                         FROM  tasks
+                        WHERE  task_stage_id IN (
+                            SELECT  stage_id 
+                              FROM  stages 
+                             WHERE  stage_module_id IN (
+                                SELECT  module_id 
+                                  FROM  modules
+                                 WHERE  module_project_id = project_id
+                        )
+                       )
+                   )
+              ) AS issues_2
+		) AS project_issues`,
+		"start_date",
+		"finish_date",
+		"start_date_fact",
+		"finish_date_fact").
+		From("projects").
+		Join("clients ON project_client_login = client_login").
+		Join("organisations ON organisation_id = client_organisation_id").
+		Join("status ON status_id = project_status_id")
 	//#--MANAGERS--#//
 	SelectManagers, _, _ = postgres.
 				Select("user_login", "user_surname", "user_name", "user_midname", "user_birthdate", "user_phone", "user_email", "role_id", "role_name").
