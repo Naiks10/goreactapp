ae54b0674ada3fcd663e00e983f5b9ad8380c394
naiks10 <ultravirus661@gmail.com>
2020-03-15 11:04:14 +0300

    Edited default port

--- main.go
+++ main.go
@@ -3,16 +3,47 @@ package main
 import (
 	"fmt"
 	"goreactapp/routes"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	path = filepath.Join(h.staticPath, path)
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
 
 //Main function => Entry point
 func main() {
 	fmt.Println("listening default port")
 
-	port := "1012" //os.Getenv("PORT") //for Heroku hosting
+	//port := "1013" //os.Getenv("PORT") //for Heroku hosting
 
 	defer func() {
 		fmt.Println("Server Stopped!")
@@ -23,8 +54,22 @@ func main() {
 
 	//r.PathPrefix("/").Handler(http.FileServer(http.Dir("./build/")))
 	r.PathPrefix("/data/").Handler(http.StripPrefix("/data/", http.FileServer(http.Dir("./data/"))))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./build/static/"))))
-	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./build/assets/"))))
-	r.HandleFunc("/", routes.IndexHandler)
-	http.ListenAndServe(":"+port, r)
+	//r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./build/static/"))))
+	//r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./build/assets/"))))
+	//r.HandleFunc("/*", routes.IndexHandler)
+
+	spa := spaHandler{staticPath: "build", indexPath: "index.html"}
+	r.PathPrefix("/").Handler(spa)
+
+	s := &http.Server{
+		Addr:           ":8085",
+		Handler:        r,
+		ReadTimeout:    1000 * time.Second,
+		WriteTimeout:   1000 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
+
+	//http.ListenAndServe(":"+port, r)
 }
